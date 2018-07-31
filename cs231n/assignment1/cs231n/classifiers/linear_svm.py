1bbe562b05cd034e56d521b820208d9d876465b6
ofirbarak <ofirbirka@gmail.com>
2018-07-31 18:51:23 +0300

    linear_svm complete

--- cs231n/assignment1/cs231n/classifiers/linear_svm.py
+++ cs231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -39,7 +39,7 @@ def svm_loss_naive(W, X, y, reg):
                 
             # update dW
             dW[:, j] += (margin > 0)*X[i]
-#         dW[:, y[i]] += -1*sum_ones*X[i]
+        dW[:, y[i]] += -1*sum_ones*X[i]
         
                 
 
@@ -73,25 +73,26 @@ def svm_loss_vectorized(W, X, y, reg):
     """
     loss = 0.0
     dW = np.zeros(W.shape) # initialize the gradient as zero
-    
+   
     #############################################################################
     # TODO:                                                                     #
     # Implement a vectorized version of the structured SVM loss, storing the    #
     # result in loss.                                                           #
     #############################################################################
+    num_classes = W.shape[1]
     num_examples = X.shape[0]
     
     scores = np.matmul(X, W)
     
     # Subtract the correct label from each example
-    true_labels_score = [scores[i, y[i]] for i in range(X.shape[0])]
-    margin = np.array(true_labels_score)
-    difference = (scores.T - margin.T).T + 1
+    examples_idx = np.arange(num_examples)
+    true_scores = scores[examples_idx, y[examples_idx]]
+
+    difference = (scores.T - true_scores.T).T + 1
     difference[difference < 0] = 0
     
     # Set the value in positions of the true labels in the difference matrix to zero
-    true_labels = np.arange(X.shape[0])
-    difference[true_labels, y[true_labels]] = 0
+    difference[examples_idx, y[examples_idx]] = 0
     
     # Sum the error, divide the number of examples and multiply by the regularization term
     loss = np.sum(difference)
@@ -111,26 +112,15 @@ def svm_loss_vectorized(W, X, y, reg):
     # to reuse some of the intermediate values that you used to compute the     #
     # loss.                                                                     #
     #############################################################################
-    C = W.shape[1]
-    D = W.shape[0]
-    N = X.shape[0]
     
-    diff = (difference > 0) * 
-    A = np.repeat(difference, np.repeat(D, C), axis=1).reshape(N,D,C)
-    A = np.sum(A, axis=0)
-    dW += A
+    # Set the sign of each weight (+1 if margin>0; -1 if margin(y_i)>0)
+    A = np.zeros(difference.shape)
+    A[difference > 0] = 1 
+    A[examples_idx, y[examples_idx]] = -np.sum(A, axis=1)
+    dW = np.matmul(X.T,A)
     
-    # y_ update
-    B = np.zeros((N,D,C))
-    
-    
-    print(A.shape)
-#     sum_ones = np.sum(difference, axis=1)
-#     xs += -1*np.dot(sum_ones, X.T).T
-
-    dW /= N
+    dW /= num_examples
     dW += 2 * reg * W
-    
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
